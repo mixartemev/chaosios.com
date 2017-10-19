e32ab6d396c7d3807eb40a3251084f65aab95175
mixartemev <mixartemev@gmail.com>
2017-10-19 11:47:00 +0300

    to yii2

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=5gnomov.ru;dbname=chaosios',
+    'dsn' => 'mysql:host=localhost;dbname=chaos',
     'username' => 'root',
-    'password' => 'mixfix99',
+    'password' => '321',
     'charset' => 'utf8',
 ];
